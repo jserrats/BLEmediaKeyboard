6fa265f4081e79e22e3b988940816d59cab36964
jserrats <jaume.serrats9@gmail.com>
2023-07-23 16:23:57 +0200

    added awful debouncing code

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,8 @@
 
 BleKeyboard bleKeyboard;
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
   pinMode(13, INPUT_PULLUP);
   pinMode(12, INPUT_PULLUP);
@@ -14,27 +15,48 @@ void setup() {
   bleKeyboard.begin();
 }
 
-void loop() {
-  if (bleKeyboard.isConnected()) {
-    if (digitalRead(13) == LOW) {
-      bleKeyboard.write(KEY_MEDIA_CONSUMER_CONTROL_CONFIGURATION);
-      //bleKeyboard.print("Hello world");
-      Serial.println("KEY_MEDIA_CONSUMER_CONTROL_CONFIGURATION");
-      delay(500);
+void loop()
+{
+  if (bleKeyboard.isConnected())
+  {
+    if (digitalRead(13) == LOW)
+    {
+      delay(100);
+      // really ugly code
+      if (digitalRead(13) == LOW)
+      {
+        bleKeyboard.write(KEY_MEDIA_CONSUMER_CONTROL_CONFIGURATION);
+        // bleKeyboard.print("Hello world");
+        delay(500);
+      }
     }
-    if (digitalRead(12) == LOW) {
-      bleKeyboard.write(KEY_MEDIA_NEXT_TRACK);
-      Serial.println("KEY_MEDIA_NEXT_TRACK");
-      delay(500);
+    if (digitalRead(12) == LOW)
+    {
+      delay(100);
+      if (digitalRead(12) == LOW)
+      {
+        bleKeyboard.write(KEY_MEDIA_NEXT_TRACK);
+        delay(500);
+      }
     }
-    if (digitalRead(14) == LOW) {
-      bleKeyboard.write(KEY_MEDIA_PLAY_PAUSE);
-      delay(500);
+    if (digitalRead(14) == LOW)
+    {
+      delay(100);
+
+      if (digitalRead(14) == LOW)
+      {
+        bleKeyboard.write(KEY_MEDIA_PLAY_PAUSE);
+        delay(500);
+      }
     }
-    if (digitalRead(27) == LOW) {
-      bleKeyboard.write(KEY_MEDIA_PREVIOUS_TRACK);
-      delay(500);
+    if (digitalRead(27) == LOW)
+    {
+      delay(100);
+      if (digitalRead(27) == LOW)
+      {
+        bleKeyboard.write(KEY_MEDIA_PREVIOUS_TRACK);
+        delay(500);
+      }
     }
   }
-  //delay(1000);
 }
